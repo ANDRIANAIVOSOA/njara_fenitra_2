ac59ec17d35f0aaa94bb9cc2cbd970717d36f9d3
19950225a <nrjmeyer@gmail.com>
2018-04-09 22:36:16 +0300

    Semi-final

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,32 @@
-#write your code here
+def translate(text)
+	word2=[]
+	words=text.split" "
+	for i in 0..words.length-1
+		word1=""
+		word=words[i]
+   			if word.start_with?('a','e','i','o','u') then 
+        		word1 = word + "ay"
+   				else 
+       				if (!word[1].start_with?('a','e','i','o','u'))  && (!word[2].start_with?('a','e','i','o','u')) then 
+         			word1 = word [3 .. word.length] + word [0 .. 2 ] + "ay"
+         		else
+           			if (!word[0].start_with?('a','e','i','o','u') && word[1].start_with?('u'))  then 
+             		word1 = word[ 2 .. word.length] + word [0 .. 1] + "ay"
+             	else 
+       				if (!word[0].start_with?('a','e','i','o','u'))  && (!word[1].start_with?('a','e','i','o','u')) && (word[2].start_with?('u')) then 
+         			word1 = word [3 .. word.length] + word [0 .. 2 ] + "ay"	 	   		
+   				else 
+           			if !word[1].start_with?('a','e','i','o','u')  then 
+             		word1 = word[ 2 .. word.length] + word [0 .. 1] + "ay"
+           			else
+                	word1 = word[ 1 .. word.length] + word [0] + "ay"
+           			end
+           		end
+       			end
+       		end
+       	end
+   	word2[i]=word1
+   	end
+   	final=word2.join" "
+   	final	
+end
